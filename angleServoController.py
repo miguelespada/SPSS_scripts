cfd79e5ebcbeae00cb38b9c070486797fdae6263
miguelespada <miguel.valero.espada@gmail.com>
2015-11-01 12:00:42 +0100

    working in servo control

--- angleServoController.py
+++ angleServoController.py
@@ -7,7 +7,7 @@ parser.add_argument('--local_port', type=int, default=7110, help='The port the O
 parser.add_argument('--ip', default='127.0.0.1', help='The ip of the remove OSC server')
 parser.add_argument('--port', type=int, default=8001, help='The port the remote OSC server ')
 parser.add_argument('--identifier', type=int, default=0, help='This is the identifier that will be sent to the visor')
-parser.add_argument('--delta_time', type=float, default=0.05, help='The port the remote OSC server ')
+parser.add_argument('--delta_time', type=float, default=0.005, help='The port the remote OSC server ')
 args = parser.parse_args()
 
 
@@ -15,7 +15,7 @@ print
 print 'Identifier: %d' % args.identifier
 print 'Listening to %s:%d' % (args.local_ip, args.local_port)
 print 'Sending to %s:%d' % (args.ip, args.port)
-print 'Sending to %f' % (args.delta_time)
+print 'Delta time %f' % (args.delta_time)
 print
 
 import RPi.GPIO as GPIO
